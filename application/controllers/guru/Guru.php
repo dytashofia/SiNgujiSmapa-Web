fae3669118b21d80239a50f420056713b526cf8a
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-04-30 01:54:22 +0700

    Menambahkan view yang akan di load oleh fungsi index dalam controller Guru, dan menambahkan komentar untuk mempermudah development

--- application/controllers/guru/Guru.php
+++ application/controllers/guru/Guru.php
@@ -1,5 +1,7 @@
 <?php
 
+// File ini berfungsi sebagai controller bagian Guru
+
     class Guru extends CI_Controller
     {
         public function __construct()
@@ -7,9 +9,20 @@
             parent::__construct();
         }
 
+        // Menampilkan home.php dalam view, berdasarkan urutan pembagian template yang 
         public function index()
         {
-            
+             // Urutan load view adalah sebagai berikut
+            // Header
+            // topNavbar
+            // sideNavbar
+            // halaman custom yang digunakan
+            // footer
+            $this->load->view('template/header');
+            $this->load->view('template/topNavbar');
+            $this->load->view('template/sideNavbar');
+            $this->load->view('guru/home');
+            $this->load->view('template/footer');
         }
 
     }
