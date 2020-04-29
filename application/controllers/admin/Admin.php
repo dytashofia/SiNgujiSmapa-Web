3d5b77c107f62cc044f2b8167fb7c60ee56e3913
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-04-30 01:37:44 +0700

    Mengubah view yang akan di load oleh function index pada controller Admin

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -6,7 +6,11 @@ class Admin extends CI_Controller {
     }
 
     public function index() {
-        $this->load->view('admin/overview');
+        $this->load->view('template/header');
+        $this->load->view('template/topNavbar');
+        $this->load->view('template/sideNavbar');
+        $this->load->view('admin/home');
+        $this->load->view('template/footer');
     }
     
 }
