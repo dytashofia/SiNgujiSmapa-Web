7b1379f3a26c8c6a7a158427e2478a36325077fe
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-04-30 01:53:38 +0700

    Memberikan komentar dalam controller Admin untuk mempermudah developmentrmudah

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -1,11 +1,20 @@
 <?php
 
+// File ini berfungsi sebagai controller bagian admin 
+
 class Admin extends CI_Controller {
     public function __construct() {
         parent:: __construct();
     }
 
+    // Menampilkan home.php dalam view, berdasarkan urutan pembagian template yang 
     public function index() {
+        // Urutan load view adalah sebagai berikut
+        // Header
+        // topNavbar
+        // sideNavbar
+        // halaman custom yang digunakan
+        // footer
         $this->load->view('template/header');
         $this->load->view('template/topNavbar');
         $this->load->view('template/sideNavbar');
