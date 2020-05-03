1476abe7dc0d2ffa3ac3b944eae1ba24f74cbb68
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-05-04 02:40:51 +0700

    Menambahkan fungsional PHP untuk menampilkan user yang sedang login

--- application/views/template/sideNavbar.php
+++ application/views/template/sideNavbar.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        <?= $_SESSION['nama_user']; ?>
                     </div>
                 </nav>
             </div>
