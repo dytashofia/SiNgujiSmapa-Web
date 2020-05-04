ada68b4206c15274c88538adde6123be65066ca9
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-05-04 08:24:21 +0700

    Memberikan logic login untuk sideNavbar.php

--- application/views/template/sideNavbar.php
+++ application/views/template/sideNavbar.php
@@ -4,7 +4,7 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <div class="sb-sidenav-menu-heading">Home</div>
                             <a class="nav-link" href="overview"
                                 ><div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard</a
@@ -53,7 +53,18 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        <?= $_SESSION['nama_user']; ?>
+                        <?php
+                            // Menguji apakah session memiliki data
+                            if(isset($_SESSION['nama_user']))
+                            {
+                                // Jika memiliki data maka tampilkan nama user
+                                echo $_SESSION['nama_user'];
+                            }else
+                            {
+                                // Jika tidak memiliki data maka jangan tampilkan apa apa
+                            }
+                        
+                        ?>
                     </div>
                 </nav>
             </div>
