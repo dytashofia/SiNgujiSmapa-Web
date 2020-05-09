ab2368595f34dde62d0d47461286cdde4ff74171
dytashofia <dytashofia32@gmail.com>
2020-05-09 22:52:33 +0700

    memindah controller soal benar salah ke bank soal pilgan

--- application/views/guru/v_pilgan.php
+++ application/views/guru/v_pilgan.php
@@ -155,7 +155,7 @@
                                         <div class="card-body">
                                         <div class="row mb-4">
                                                 <div class="col-sm-12 col-md-4">
-                                                    <a href="<?= base_url('guru/C_soalEssay/tambah_benarSalah');?>" class="btn btn-outline-success">Tambah Soal</a>
+                                                    <a href="<?= base_url('guru/pilgan/tambah_benarSalah');?>" class="btn btn-outline-success">Tambah Soal</a>
                                                 </div>
                                         </div>
                                             <div class="table-responsive">
@@ -182,12 +182,12 @@
                                                                 <td>
                                                                     <div class="row">
                                                                         <div class="col-sm-12 col-md-2 mr-3">
-                                                                            <a href="<?= base_url('guru/C_soalEssay/edit_benarSalah/'.$benarSalah->id_soal);?>" class="btn btn-primary">
+                                                                            <a href="<?= base_url('guru/pilgan/edit_benarSalah/'.$benarSalah->id_soal);?>" class="btn btn-primary">
                                                                                 <i class="fas fa-pen"></i>
                                                                             </a>
                                                                         </div>
                                                                         <div class="col-sm-12 col-md-2 mr-3">
-                                                                            <a href="<?= base_url('guru/C_soalEssay/hapus_benarSalah/'.$benarSalah->id_soal);?>" class="btn btn-danger">
+                                                                            <a href="<?= base_url('guru/pilgan/hapus_benarSalah/'.$benarSalah->id_soal);?>" class="btn btn-danger">
                                                                                 <i class="fas fa-trash-alt"></i>
                                                                             </a>
                                                                         </div>
