ed534e7c5977f54d1d4093b5674132dfb947c521
Dinda Ayu Nafisyah E41181807 <dinda.nafi1999@gmail.com>
2020-05-12 08:02:21 +0700

    Update m_data_soal.php

--- application/models/m_data_soal.php
+++ application/models/m_data_soal.php
@@ -39,7 +39,7 @@ class M_data_soal extends CI_Model{
   }
    //function untuk mengedit data soal
   function edit_data($where,$table){		
-    return $this->db->get_where($table,$where);
+    return $this->db->get($table,$where);
   }
   //function untuk mengupdate tabel soal setelah dilakukan edit data
   function update_data($where,$data,$table){
@@ -48,7 +48,7 @@ class M_data_soal extends CI_Model{
   }	
   //function untuk melihat data soal yang diinginkan 
   function lihat_data($where,$table){		
-    return $this->db->get_where($table,$where);
+    return $this->db->get($table,$where);
   }
 
   // function untuk melihat semua data dalam tabel paket soal
