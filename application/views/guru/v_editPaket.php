e50458b82ff9bd8d4565e74690f86da4029c2b2e
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-05-13 17:56:03 +0700

    Mengubah link redirect sesuai dengan route pada routes.php

--- application/views/guru/v_editPaket.php
+++ application/views/guru/v_editPaket.php
@@ -6,8 +6,8 @@
                 <i class="fa fa-table mr-2"></i>Tambah Paket Soal
             </h1>
             <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item active"><a href="">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="<?= base_url('guru/pilgan/tampilPaket');?>">Paket Soal</a></li>
+                <li class="breadcrumb-item active"><a href="<?= base_url('guru');?>">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="<?= base_url('tampilPaket');?>">Paket Soal</a></li>
                 <li class="breadcrumb-item active">Tambah Paket Soal</li>
             </ol>
             <div class="row mt-2">
@@ -71,7 +71,7 @@
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-sm-12 col-md-2">
-                                                <a href="<?= base_url('/guru/pilgan/tampilPaket/');?>" class="btn btn-outline-secondary p-2 w-100">Kembali</a>
+                                                <a href="<?= base_url('tampilPaket');?>" class="btn btn-outline-secondary p-2 w-100">Kembali</a>
                                             </div>
                                             <div class="col-sm-12 col-md-2">
                                                 <button type="submit" class="btn btn-primary p-2 w-100">Simpan</button>
