5f3754be064d5a623168fabd973deea1e46e3119
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-05-13 07:26:59 +0700

    Membuat route route baru, memisahkan route untuk login, guru, dan lainnya.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'login/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
@@ -61,9 +61,17 @@ $route['translate_uri_dashes'] = FALSE;
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
 $route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
 
-$route['admin'] = 'admin/admin';
+// Route untuk melakukan login
+$route['loginGuru'] = "login/login";
+$route['loginAdmin'] = "login/loginadm";
+
+// Route untuk guru
 $route['guru'] = 'guru/guru';
-$route['loginGuru']= 'login/login';
-$route['loginAdmin'] = 'login/loginadm';
-$route['pilgan'] = 'guru/pilgan';
-$route['essay'] = 'guru/C_soalEssay';
+// Route untuk paket soal
+$route['tampilPaket'] = 'guru/pilgan/tampilPaket';
+$route['tambahPaket'] = 'guru/pilgan/tambahPaket';
+$route['detailPaket/(:any)'] = 'guru/pilgan/tampilDetailPaket/$1';
+$route['editPaket/(:any)'] = 'guru/pilgan/editPaket/$1';
+$route['hapusPaket/(:any)'] = 'guru/pilgan/hapusPaket/$1';
+
+// Route
