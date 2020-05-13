28ff264d42824b4e93e13cad94b4e51c44bb0e0c
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-05-13 07:35:15 +0700

    Merubah side navbar

--- application/views/template/sideNavbar.php
+++ application/views/template/sideNavbar.php
@@ -16,7 +16,9 @@
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div
                             ></a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="C_soalEssay">Soal</a>
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?= base_url('tampilPaket');?>">Paket Soal</a>
+                                </nav>
                             </div>
                           
                             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages"
@@ -42,14 +44,6 @@
                                     </div>
                                 </nav>
                             </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html"
-                                ><div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts</a
-                            ><a class="nav-link" href="tables.html"
-                                ><div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables</a
-                            >
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
