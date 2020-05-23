9160d1aebc936fc029907985d11a9868878b6d07
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-05-23 07:07:18 +0700

    update controller admin.php
    menambahkan pengecekan sesion status login admin

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -5,6 +5,11 @@
 class Admin extends CI_Controller {
     public function __construct() {
         parent:: __construct();
+
+        // mengecek apakah sesion status, untuk mendeteksi apakah user atau admin sudah login atau belum. 
+        if($this->session->userdata('status') != "login"){ 
+            redirect(base_url("login")); //jika user tidak berhasil login maka akan diarahkan ke halaamn login
+        }
     }
 
     // Menampilkan home.php dalam view, berdasarkan urutan pembagian template yang 
