fe79ffe760f7bc3f23fcc72a3460e1aa2b28788f
dytashofia <dytashofia32@gmail.com>
2020-06-02 13:38:00 +0700

    Create login_siswa.php
    kerangka rest api login siswa

---
+++ application/controllers/login/login_siswa.php
@@ -0,0 +1,12 @@
+<?php
+
+/* === Controller ini digunakan sebagai REST API android login siswa === */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';  // import library dari rest controller
+use Restserver\Libraries\REST_Controller; 
+
+
+
+?>
