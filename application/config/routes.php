f97c75d4322788f19c97aea1ca8bf4862eec4c40
ummalatulkamilah <55658527+ummalatulkamilah@users.noreply.github.com>
2020-06-27 09:28:42 +0700

    menambah route untuk master jurusan

--- application/config/routes.php
+++ application/config/routes.php
@@ -61,6 +61,7 @@ $route['translate_uri_dashes'] = FALSE;
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
 $route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
 
+$route['jurusan'] = "admin/crudjurusan";
 // Route untuk melakukan login
 $route['loginGuru'] = "login/login";
 $route['loginAdmin'] = "login/loginadm";
