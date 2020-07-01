102cbd6afee2c79dd9b6e685c974dd72527d6ecc
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-07-01 09:19:44 +0700

    Bug fix, tampilan paket soal

--- application/views/guru/v_tambah_paketSoal.php
+++ application/views/guru/v_tambah_paketSoal.php
@@ -50,16 +50,6 @@
                                         endforeach;
                                         ?>
                                     </select>
-                                    <div class="form-group">
-                                        <label>ID Transportasi</label>
-                                        <select name="id_transport" class="form-control">
-                                            <option value="">--Pilih Transport--</option>
-                                            <?php foreach ($transport as $sport) : ?>
-                                                <option value="<?php echo $sport->id_transport; ?>"> <?php echo $sport->id_transport; ?>
-                                                </option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    </div>
                                     <?= form_error('id_mapel', '<small class="text-form text-danger mt-2 ml-2">', '</small>'); ?>
                                 </div>
                                 <div class="form-group">
@@ -86,4 +76,3 @@
 
         </div>
     </main>
-</div>
