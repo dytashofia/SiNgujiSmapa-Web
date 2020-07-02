ff451933718340104e59414da6938ba291969a96
bagoesihsant <61548001+bagoesihsant@users.noreply.github.com>
2020-07-03 04:23:38 +0700

    Menambahkan model exam dan controller exam untuk API ujian di android

--- application/models/m_exam.php
+++ application/models/m_exam.php
@@ -4,7 +4,28 @@
 
     class M_Exam extends CI_Model
     {
-        
+
+        public function get_soal($data)
+        {
+            return $this->db->get_where('tb_soal', $data);
+        }
+
+        public function get_total_jawaban()
+        {
+            return $this->db->get('tb_jawaban');
+        }
+
+        public function get_last_jawaban()
+        {
+            $this->db->order_by('id_jawaban', 'DESC');
+            return $this->db->get('tb_jawaban', 1);
+        }
+
+        public function tambah_jawaban($data)
+        {
+            $this->db->insert("tb_jawaban", $data);
+        }
+
     }
 
 ?>
